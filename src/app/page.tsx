80ce42dda72d225e9a4932bd162eeac184f92934
Brad Anderson <>
2024-03-24 22:38:41 -0400

    Adds revalidation for posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { BlogPosts } from '@/components/BlogPosts';
 import { Header } from '@/components/Header';
 
 export default async function Home() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API}/posts`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API}/posts`, {
+    next: { tags: ['posts'] },
+  })
   const { posts } = await response.json()
 
   return (
